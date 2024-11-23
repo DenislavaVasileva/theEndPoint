9e1953d3dd539b2ac70021095cee5dbd1818bdee
DenislavaVasileva <idiototavur@gmail.com>
2024-11-23 17:47:47 +0200

    Added PeakDetailView and peak_details template

--- theEndPoint/peaks/urls.py
+++ theEndPoint/peaks/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
-from theEndPoint.peaks.views import PeakListView
+from django.urls import path, include
+from theEndPoint.peaks.views import PeakListView, PeakDetailView
 
 urlpatterns = [
     path('', PeakListView.as_view(), name='peaks_dashboard'),
+    path('<int:peak_id>/', include([
+        path('details/', PeakDetailView.as_view(), name='details-peak'),
+    ]))
 ]
+
--- theEndPoint/peaks/views.py
+++ theEndPoint/peaks/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from theEndPoint.peaks.models import Peak
 
 
@@ -8,4 +7,10 @@ class PeakListView(ListView):
     template_name = 'peaks/peaks_dashboard.html'
     context_object_name = 'peaks'
     ordering = ['-elevation']
+
+
+class PeakDetailView(DetailView):
+    model = Peak
+    template_name = 'peaks/peak_details.html'
+    pk_url_kwarg = 'peak_id'
 
