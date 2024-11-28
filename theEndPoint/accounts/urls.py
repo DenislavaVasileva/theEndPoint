5a1244e9213847618dc7010ccf097c75a3a2580b
DenislavaVasileva <idiototavur@gmail.com>
2024-11-28 14:58:33 +0200

    Added a README

--- theEndPoint/accounts/urls.py
+++ theEndPoint/accounts/urls.py
@@ -2,15 +2,6 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from theEndPoint.accounts.views import UserRegisterView, DetailProfileView, EditProfileView
 
-# urlpatterns = [
-#     path('register/', UserRegisterView.as_view(), name='register'),
-#     path('login/', LoginView.as_view(), name='login'),
-#     path('logout/', LogoutView.as_view(), name='logout'),
-#     path('profile/<int:pk>/', include([
-#         path('details/', DetailProfileView.as_view(), name='profile_details'),
-#         path('edit/', EditProfileView.as_view(), name='profile_edit')
-#     ])),
-# ]
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
